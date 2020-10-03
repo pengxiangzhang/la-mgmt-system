09ba88e962f561fa6937939cbc9776ffb7e05773
Pengxiang Zhang <z-git@protonmail.com>
2020-10-02 19:26:38 -0500

    implement session control

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,2 +1,8 @@
 class AdminController < ApplicationController
+  before_action :check_admin
+  def check_admin
+  if user_type!="admin"
+    redirect_to '/'
+  end
+  end
 end
--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,2 +1,8 @@
 class StudentController < ApplicationController
+  before_action :check_student
+  def check_student
+  if user_type!="student"
+    redirect_to '/'
+  end
+  end
 end
