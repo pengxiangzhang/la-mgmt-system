4b2b110a2c1d5d20227d18bc83a444f2da06fb95
ngerstbrein <ngerstbrein@huskers.unl.edu>
2020-10-06 14:25:35 -0500

    Added backend functionality for the AdminController

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,2 +1,18 @@
 class AdminController < ApplicationController
+
+
+  def index
+    @users= UserDetail.all
+  end
+
+  def show
+    @user= UserDetail.find(params[:id])
+  end
+
+   def change
+     user= UserDetail.first(:conditions => ["eduPersonPrincipalName = ?", [params: eduPersonPrincipalName ]])
+     user.role("LA")
+     user.save
+   end
+
 end
