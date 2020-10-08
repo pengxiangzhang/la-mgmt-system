bfef6e8ae132ddce9beb1834da6e17edc4ba8d6b
Pengxiang Zhang <z-git@protonmail.com>
2020-10-08 16:23:59 -0500

    Complete view design for LA and admin

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -2,7 +2,7 @@ class StudentController < ApplicationController
   before_action :check_student
   def check_student
   if user_type!="student"
-    redirect_to '/'
+    # redirect_to '/'
   end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   get 'home/index'
   get 'student/index'
+  get 'student/request'
   get 'la/index'
+  get 'la/settings'
+  get 'la/request'
   get 'admin/index'
+  get 'admin/courses'
+  get 'admin/evaluations'
+  get 'admin/hiring'
+  get 'admin/management'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
