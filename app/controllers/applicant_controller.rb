f7515ab83780c56a3cb9ca1af2c0eb8b7e3f43fa
Pengxiang Zhang <z-git@protonmail.com>
2020-10-29 02:37:11 -0500

    Initial form edit check before save

--- app/controllers/applicant_controller.rb
+++ app/controllers/applicant_controller.rb
@@ -12,14 +12,14 @@ class ApplicantController < ApplicationController
     @submit = params
     filename = params[:NUID] + Time.now.strftime("-%d%m%Y")
     p filename
-    render "applicant/pdf"
-    # render pdf: "/Users/pengxiangzhang/Desktop/test",
-    #        :save_to_file => Rails.root.join('application', filename + ".pdf"),
-    #        page_size: 'letter',
-    #        template: "applicant/pdf.html.erb",
-    #        layout: "pdf.html",
-    #        lowquality: true,
-    #        zoom: 1,
-    #        dpi: 75
+    # render "applicant/pdf"
+    render pdf: "/Users/pengxiangzhang/Desktop/test",
+           :save_to_file => Rails.root.join('storage/application', filename + ".pdf"),
+           page_size: 'letter',
+           template: "applicant/pdf.html.erb",
+           layout: "pdf.html",
+           lowquality: true,
+           zoom: 1,
+           dpi: 75
   end
 end
