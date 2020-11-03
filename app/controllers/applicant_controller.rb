167821293fc704990717d21de225f5ee86142285
Pengxiang Zhang <z-git@protonmail.com>
2020-11-03 15:16:49 -0600

    Implement method to merge mutiable pdf file.

--- app/controllers/applicant_controller.rb
+++ app/controllers/applicant_controller.rb
@@ -7,33 +7,27 @@ class ApplicantController < ApplicationController
 
   def create
     @submit = params
+
+    filename = params[:NUID] + Time.now.strftime("-%Y%m%d%H%M%S")
+    Application.new(NUID: params[:NUID], Name: params[:Name], Email: params[:Email], Course: '', GPA: params[:GPA], file_location: "storage/application/" + filename + ".pdf", application_status: "submitted").save
+    EmailMailer.new_applicant(@submit).deliver_now
+    EmailMailer.thank_applying(@submit).deliver_now
+    pdf = WickedPdf.new.pdf_from_string(render_to_string(:template => 'applicant/pdf.html.erb', :layout => 'pdf.html.erb', :page_size => 'letter', :lowquakity => true, :zoom => 1, :dpi => 75))
+    save_path = Rails.root.join('storage/application', filename + ".pdf")
+    File.open(Rails.root.join('tmp/pdf/', "tmp.pdf"), 'wb') do |file|
+      file << pdf
+    end
+    pdf = CombinePDF.new
+    pdf << CombinePDF.load(Rails.root.join("tmp/pdf/", "tmp.pdf"))
     if params[:File].present?
       (params[:File] || []).each do |muti|
-        File.open(Rails.root.join('storage/application', muti.original_filename), 'wb') do |file|
+        File.open(Rails.root.join('tmp/pdf/', "tmp.pdf"), 'wb') do |file|
           file.write(muti.read)
+          pdf << CombinePDF.load(Rails.root.join("tmp/pdf/", "tmp.pdf"))
         end
       end
     end
-
-    filename = params[:NUID] + Time.now.strftime("-%Y%m%d%H%M%S")
-    # Application.new(NUID: params[:NUID], Name: params[:Name], Email: params[:Email], Course: '', GPA: params[:GPA], file_location: "storage/application/" + filename + ".pdf", application_status: "submitted").save
-    # EmailMailer.new_applicant(@submit).deliver_now
-    # EmailMailer.thank_applying(@submit).deliver_now
-    app1 = WickedPdf.new.pdf_from_string(render_to_string(pdf: 'report1', :template => 'applicant/pdf.html.erb', :layout => 'pdf.html.erb', :page_size => 'letter', :lowquakity => true, :zoom => 1, :dpi => 75))
-    combiner = CombinePDF.new
-    combiner << CombinePDF.parse(app1)
-    combiner.to_pdf
-    save_path = Rails.root.join('storage/application', filename + ".pdf")
-    File.open(save_path, 'wb') do |file|
-      file << combiner
-    end
+    pdf.save save_path
     render "applicant/confirm"
-  end
-
-  def ddd
-    uploaded_io = params[:picture]
-    File.open(Rails.root.join('storage/application', uploaded_io.original_filename), 'wb') do |file|
-      file.write(uploaded_io.read)
-    end
   end
 end
