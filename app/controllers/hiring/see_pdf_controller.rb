c534629729d6931b4b3c8356d01af79f443fb328
Pengxiang Zhang <z-git@protonmail.com>
2020-11-18 00:18:48 -0600

    Beautify code

--- app/controllers/hiring/see_pdf_controller.rb
+++ app/controllers/hiring/see_pdf_controller.rb
@@ -3,9 +3,9 @@ class Hiring::SeePdfController < ApplicationController
 
   def create
     @applicant = Application.where(:NUID => params[:nuid]).where(:Application_Status => params[:status]).first
-        send_file("#{Rails.root}/" + @applicant.File_Location,
-                  :filename => "LA Application - " + @applicant.NUID + ".pdf",
-                  :type => "application/pdf", #for example if pdf
-                  :disposition => 'inline')
+    send_file("#{Rails.root}/" + @applicant.File_Location,
+              :filename => "LA Application - " + @applicant.NUID + ".pdf",
+              :type => "application/pdf", #for example if pdf
+              :disposition => 'inline')
   end
 end
