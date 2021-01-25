c34b052829acd29edc527dfde8cbddc1eff3ae7f
Pengxiang Zhang <z-git@protonmail.com>
2021-01-25 14:52:59 -0600

    Change Tmp file using uuid

--- app/controllers/applicant/withdraw_controller.rb
+++ app/controllers/applicant/withdraw_controller.rb
@@ -1,6 +1,6 @@
 class Applicant::WithdrawController < ApplicationController
 
-  def delete
+  def create
     @application = Application.where(eduPersonPrincipalName: cas_user).where.not(Application_Status: "withdraw").where.not(Application_Status: "delete").first
     @application.Application_Status = "withdraw"
     @application.save
