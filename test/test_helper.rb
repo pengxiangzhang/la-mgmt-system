67751f4fa1da52390da57fb99a7775241b22bb48
Pengxiang Zhang <z-git@protonmail.com>
2021-01-28 17:27:10 -0600

    Beautify code

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,6 @@ require_relative '../config/environment'
 require 'rails/test_help'
 require 'webdrivers'
 
-
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
