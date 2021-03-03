1602aeeecd69d56a22d34b3e2c00fdc9ff6c118f
Pengxiang Zhang <z-git@protonmail.com>
2021-03-03 17:06:04 -0600

    Test new schedular

--- db/schema.rb
+++ db/schema.rb
@@ -104,10 +104,10 @@ ActiveRecord::Schema.define(version: 2021_02_12_234443) do
 
   create_table "user_details", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "eduPersonPrincipalName"
-    t.string "DisplayName"
-    t.string "Email"
-    t.string "Role"
-    t.boolean "hasAppointment"
+    t.string "DisplayName", default: "NULL"
+    t.string "Email", default: "NULL"
+    t.string "Role", default: "Student"
+    t.boolean "hasAppointment", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
