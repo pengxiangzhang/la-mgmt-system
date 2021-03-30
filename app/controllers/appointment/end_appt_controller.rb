bae9c9fd7c9c4bef14be27ee262a67e03d960815
Pengxiang Zhang <z-git@protonmail.com>
2021-03-29 22:27:10 -0500

    bug fix on not sending out survey

--- app/controllers/appointment/end_appt_controller.rb
+++ app/controllers/appointment/end_appt_controller.rb
@@ -5,22 +5,23 @@ class Appointment::EndApptController < ApplicationController
     @appointment = Appointment.find_by({ id: params['id'] })
     if !((@appointment.status == 'Started') || (@appointment.la_eduPersonPrincipalName != cas_user) || (@appointment.eduPersonPrincipalName != cas_user))
       flash[:info] = 'You are not allowed to do this action'
-      redirect_to(:back)
+      render json: {}, status: 400
     else
       UserDetail.find_by(eduPersonPrincipalName: @appointment.eduPersonPrincipalName).update(hasAppointment: false)
       @appointment.status = 'Closed'
       @appointment.endTime = Time.now
       @appointment.close_reason = "Appointment ended by: #{cas_name}"
       @appointment.save
-      begin
-        send_interaction(@appointment.eduPersonPrincipalName, @appointment.la_eduPersonPrincipalName, @appointment.class_id, 'appointment')
+      feedback = send_interaction(@appointment.eduPersonPrincipalName, @appointment.la_eduPersonPrincipalName, @appointment.class_id, 'appointment')
+      if feedback == true
         flash[:success] = 'Appointment Ended'
         ActionLogger.info("[User: #{cas_user}|IP:#{request.ip}|End Appointment] Appointment end for ID: '#{@appointment.id}'.")
-      rescue StandardError
+      else
         flash[:info] = "User not found at the Survey system: #{@appointment.eduPersonPrincipalName}. Please report this to the admin."
         ActionLogger.info("[User: #{cas_user}|IP:#{request.ip}|End Appointment] Appointment end for ID: '#{@appointment.id}' No Student find in survey system.")
       end
-      redirect_to(:back)
+      sleep 1
+      render json: {}, status: 200
     end
   end
 end
