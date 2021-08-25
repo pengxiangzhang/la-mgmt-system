55a3896e8856e3acf2e82baa56ab78b4b2a47611
Pengxiang Zhang <z-git@protonmail.com>
2021-08-24 22:00:51 -0500

    DAte to date update

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2021_04_17_185941) do
 
+  create_table "announcements", primary_key: "announcement_key", id: :integer, charset: "utf8mb4", force: :cascade do |t|
+    t.string "course", limit: 20
+    t.string "class", limit: 20
+    t.string "body", limit: 500
+    t.index ["course"], name: "course", unique: true
+  end
+
   create_table "applications", charset: "utf8", force: :cascade do |t|
     t.string "eduPersonPrincipalName"
     t.string "NUID"
@@ -57,11 +64,45 @@ ActiveRecord::Schema.define(version: 2021_04_17_185941) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "cse_usernames", primary_key: "username_key", id: :integer, charset: "utf8mb4", force: :cascade do |t|
+    t.string "username", limit: 20
+    t.string "canvas_username", limit: 20
+    t.string "name", limit: 70
+    t.string "course", limit: 10
+    t.string "email", limit: 100
+    t.boolean "is_admin", default: false
+    t.index ["username", "canvas_username", "course"], name: "cse_usernames_uindex", unique: true
+  end
+
+  create_table "feedback", primary_key: "feedback_key", id: :integer, charset: "utf8mb4", force: :cascade do |t|
+    t.integer "interaction_key", null: false
+    t.integer "rating", limit: 1, default: 0
+    t.string "comment", limit: 500
+    t.integer "sentiment"
+    t.boolean "desires_feedback"
+    t.integer "time_to_complete"
+    t.index ["interaction_key"], name: "interaction_key", unique: true
+  end
+
   create_table "form_builders", charset: "utf8", force: :cascade do |t|
     t.string "formname"
     t.text "formdata"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "interactions", primary_key: "interaction_key", id: :integer, charset: "utf8mb4", force: :cascade do |t|
+    t.integer "la_username_key", null: false
+    t.integer "student_username_key", null: false
+    t.string "course", limit: 10
+    t.string "interaction_type", limit: 30
+    t.boolean "seeking_feedback", default: false, null: false
+    t.boolean "has_received_feedback", default: false
+    t.timestamp "time_of_interaction", default: -> { "current_timestamp()" }, null: false
+    t.boolean "was_recommended", default: false, null: false
+    t.index ["interaction_key"], name: "interactions_interaction_key_uindex", unique: true
+    t.index ["la_username_key"], name: "interactions_la_fk"
+    t.index ["student_username_key"], name: "interactions_student_fk"
   end
 
   create_table "la_courses", charset: "utf8", force: :cascade do |t|
@@ -100,6 +141,12 @@ ActiveRecord::Schema.define(version: 2021_04_17_185941) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "logins", primary_key: "login_key", id: :integer, charset: "utf8mb4", force: :cascade do |t|
+    t.integer "la_username_key", null: false
+    t.timestamp "time_of_interaction", default: -> { "current_timestamp()" }, null: false
+    t.index ["la_username_key"], name: "interactions_la_login_fk"
+  end
+
   create_table "sessions", id: :integer, charset: "utf8", force: :cascade do |t|
     t.string "session_id", null: false
     t.string "cas_ticket", limit: 1000
@@ -125,7 +172,11 @@ ActiveRecord::Schema.define(version: 2021_04_17_185941) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "feedback", "interactions", column: "interaction_key", primary_key: "interaction_key", name: "feedback_interaction_fk", on_delete: :cascade
+  add_foreign_key "interactions", "cse_usernames", column: "la_username_key", primary_key: "username_key", name: "interactions_la_fk", on_delete: :cascade
+  add_foreign_key "interactions", "cse_usernames", column: "student_username_key", primary_key: "username_key", name: "interactions_student_fk", on_delete: :cascade
   add_foreign_key "la_courses", "courses"
   add_foreign_key "la_courses", "la_details"
   add_foreign_key "la_details", "user_details"
+  add_foreign_key "logins", "cse_usernames", column: "la_username_key", primary_key: "username_key", name: "interactions_la_login_fk", on_delete: :cascade
 end
